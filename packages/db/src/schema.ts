886af9ad83b5c05f9ae0547eff60e295a682a605
Davi Dias <davifernandosousadias18@gmail.com>
2025-03-08 15:13:02 -0300

    fix: refactoring schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -4,7 +4,7 @@ import {
   text,
   timestamp,
   varchar,
-  doublePrecision
+  doublePrecision,
 } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
@@ -22,15 +22,25 @@ export const products = pgTable("products", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   nome: varchar("nome", { length: 255 }).notNull(),
   price: doublePrecision("price").notNull(),
-  color: text("color"),
-  assessment: doublePrecision("assessment"),
   discount: doublePrecision("discount"),
   image: text("image"),
-  amount: integer().notNull().default(0),
+  type: varchar("type", { length: 100 }),
+  description: varchar("description", { length: 255 }),
+  assessment: doublePrecision("assessment").notNull(),
   user_id: text("user_id").references(() => users.id, {
     onDelete: "set null",
     onUpdate: "cascade",
   }),
+});
+
+export const productVariants = pgTable("product_variants", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  product_id: integer("product_id")
+    .notNull()
+    .references(() => products.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  size: varchar("size", { length: 55 }).notNull(), 
+  color: text("color").notNull(), 
+  quantity: integer("quantity").notNull().default(0), 
 });
 
 export const carts = pgTable("carts", {
@@ -38,20 +48,19 @@ export const carts = pgTable("carts", {
   user_id: text("user_id")
     .notNull()
     .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
-  product_id: integer("product_id")
+  product_variant_id: integer("product_variant_id")
     .notNull()
-    .references(() => products.id, { onDelete: "cascade", onUpdate: "cascade" }),
-  quantity: integer("quantity").notNull().default(1), 
+    .references(() => productVariants.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
   created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
 });
-
 
 export const orders = pgTable("orders", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   user_id: text("user_id")
     .notNull()
     .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
-  status: varchar("status", { length: 50 }).default("pending"), 
+  status: varchar("status", { length: 50 }).default("pending"),
   created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
 });
 
@@ -60,10 +69,22 @@ export const orderItems = pgTable("order_items", {
   order_id: integer("order_id")
     .notNull()
     .references(() => orders.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  product_variant_id: integer("product_variant_id")
+    .notNull()
+    .references(() => productVariants.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  quantity: integer("quantity").notNull().default(1),
+});
+
+export const reviews = pgTable("reviews", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  user_id: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
   product_id: integer("product_id")
     .notNull()
     .references(() => products.id, { onDelete: "cascade", onUpdate: "cascade" }),
-  quantity: integer("quantity").notNull().default(1),
+  feedback_date: timestamp("feedback_date", { mode: "date" }).defaultNow(),
+  comment: text("comment").notNull(),
 });
 
 export const insertUserSchema = createInsertSchema(users);
@@ -71,6 +92,9 @@ export const selectUserSchema = createSelectSchema(users);
 
 export const insertProductSchema = createInsertSchema(products);
 export const selectProductSchema = createSelectSchema(products);
+
+export const insertProductVariantSchema = createInsertSchema(productVariants);
+export const selectProductVariantSchema = createSelectSchema(productVariants);
 
 export const insertCartSchema = createInsertSchema(carts);
 export const selectCartSchema = createSelectSchema(carts);
@@ -80,3 +104,6 @@ export const selectOrderSchema = createSelectSchema(orders);
 
 export const insertOrderItemSchema = createInsertSchema(orderItems);
 export const selectOrderItemSchema = createSelectSchema(orderItems);
+
+export const insertReviewSchema = createInsertSchema(reviews);
+export const selectReviewSchema = createSelectSchema(reviews);
